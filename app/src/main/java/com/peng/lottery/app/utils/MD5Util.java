00e3aa587c6b7f49061394bccdf48da3e9fbb35d
Peng <1136313078@qq.com>
2019-01-14 16:02:02 +0800

    增加获取幸运号码功能

---
+++ app/src/main/java/com/peng/lottery/app/utils/MD5Util.java
@@ -0,0 +1,35 @@
+package com.peng.lottery.app.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * MD5加密的工具类
+ */
+public class MD5Util {
+    /**
+     * 进行md5的加密运算
+     */
+    public static String encode(String str) {
+        // MessageDigest专门用于加密的类
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] result = messageDigest.digest(str.getBytes()); // 得到加密后的字符组数
+            StringBuffer sb = new StringBuffer();
+            for (byte b : result) {
+                int num = b & 0xff; // 这里的是为了将原本是byte型的数向上提升为int型，从而使得原本的负数转为了正数
+                String hex = Integer.toHexString(num); //这里将int型的数直接转换成16进制表示
+                //16进制可能是为1的长度，这种情况下，需要在前面补0，
+                if (hex.length() == 1) {
+                    sb.append(0);
+                }
+                sb.append(hex);
+            }
+            return sb.toString().toUpperCase();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
+
--- app/src/main/java/com/peng/lottery/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/peng/lottery/mvp/ui/activity/MainActivity.java
@@ -1,14 +1,19 @@
 package com.peng.lottery.mvp.ui.activity;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.peng.lottery.R;
+import com.peng.lottery.app.utils.MD5Util;
+import com.peng.lottery.app.utils.ToastUtil;
 import com.peng.lottery.base.SimpleBaseActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -16,8 +21,12 @@ import butterknife.BindView;
 
 public class MainActivity extends SimpleBaseActivity {
 
-    @BindView(R.id.bt_get_number)
-    Button btGetNumber;
+    @BindView(R.id.et_lucky_str)
+    EditText etLuckyStr;
+    @BindView(R.id.bt_get_lucky_number)
+    Button btGetLuckyNumber;
+    @BindView(R.id.bt_get_random_number)
+    Button btGetRandomNumber;
     @BindView(R.id.tv_red_one)
     TextView tvRedOne;
     @BindView(R.id.tv_red_two)
@@ -35,9 +44,8 @@ public class MainActivity extends SimpleBaseActivity {
     @BindView(R.id.layout_lottery_number)
     LinearLayout layoutLotteryNumber;
 
-    private List<String> mNumberBox = new ArrayList<>();
-    private String[] mRedBallNumbers = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"};
-    private String[] mBlueBallNumbers = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
+    private static String[] mRedBallNumbers = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"};
+    private static String[] mBlueBallNumbers = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
 
     @Override
     protected int setLayoutResID() {
@@ -46,33 +54,79 @@ public class MainActivity extends SimpleBaseActivity {
 
     @Override
     protected void initListener() {
-        btGetNumber.setOnClickListener(v -> {
-            if (layoutLotteryNumber.getVisibility() == View.GONE) {
-                layoutLotteryNumber.setVisibility(View.VISIBLE);
+        btGetLuckyNumber.setOnClickListener(v -> {
+            String luckyStr = etLuckyStr.getText().toString().trim();
+            if (TextUtils.isEmpty(luckyStr)) {
+                ToastUtil.showToast(mActivity, "请先输入一些内容吧！");
+                return;
             }
 
-            mNumberBox.clear();
-            tvRedOne.setText(getRandomNumber(mRedBallNumbers));
-            tvRedTwo.setText(getRandomNumber(mRedBallNumbers));
-            tvRedThree.setText(getRandomNumber(mRedBallNumbers));
-            tvRedFour.setText(getRandomNumber(mRedBallNumbers));
-            tvRedFive.setText(getRandomNumber(mRedBallNumbers));
-            mNumberBox.clear();
-            tvBlueOne.setText(getRandomNumber(mBlueBallNumbers));
-            tvBlueTwo.setText(getRandomNumber(mBlueBallNumbers));
+            checkShowLayout();
+            setDataToLayout(getRedNumbers(true, luckyStr), getBlueNumbers(true, luckyStr));
+        });
+        btGetRandomNumber.setOnClickListener(v -> {
+            checkShowLayout();
+            setDataToLayout(getRedNumbers(false, ""), getBlueNumbers(false, ""));
         });
     }
 
-    private String getRandomNumber(String[] numbers) {
-        String number = "";
-        boolean flag = true;
-        while (flag){
-            number = numbers[new Random().nextInt(numbers.length)];
-            if (!mNumberBox.contains(number)) {
-                mNumberBox.add(number);
-                flag = false;
+    private void checkShowLayout() {
+        if (layoutLotteryNumber.getVisibility() == View.GONE) {
+            layoutLotteryNumber.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setDataToLayout(List<String> redNumberBox, List<String> blueNumberBox) {
+        if (redNumberBox.size() == 5) {
+            tvRedOne.setText(redNumberBox.get(0));
+            tvRedTwo.setText(redNumberBox.get(1));
+            tvRedThree.setText(redNumberBox.get(2));
+            tvRedFour.setText(redNumberBox.get(3));
+            tvRedFive.setText(redNumberBox.get(4));
+        }
+        if (blueNumberBox.size() == 2) {
+            tvBlueOne.setText(blueNumberBox.get(0));
+            tvBlueTwo.setText(blueNumberBox.get(1));
+        }
+    }
+
+    private List<String> getRedNumbers(boolean isLucky, String luckyStr) {
+        List<String> numberBox = new ArrayList<>();
+        int luckyIndex = 0;
+        byte[] luckyByte = MD5Util.encode(luckyStr).getBytes();
+        while (numberBox.size() < 5) {
+            String number;
+            if (isLucky) {
+                number = mRedBallNumbers[luckyByte[luckyIndex]%35];
+                luckyIndex += 2;
+            } else {
+                number = mRedBallNumbers[new Random().nextInt(mRedBallNumbers.length)];
+            }
+            if (!numberBox.contains(number)) {
+                numberBox.add(number);
             }
         }
-        return number;
+        Collections.sort(numberBox);
+        return numberBox;
+    }
+
+    private List<String> getBlueNumbers(boolean isLucky, String luckyStr) {
+        List<String> numberBox = new ArrayList<>();
+        int luckyIndex = 16;
+        byte[] luckyByte = MD5Util.encode(luckyStr).getBytes();
+        while (numberBox.size() < 2) {
+            String number;
+            if (isLucky) {
+                number = mBlueBallNumbers[luckyByte[luckyIndex]%12];
+                luckyIndex += 3;
+            } else {
+                number = mBlueBallNumbers[new Random().nextInt(mBlueBallNumbers.length)];
+            }
+            if (!numberBox.contains(number)) {
+                numberBox.add(number);
+            }
+        }
+        Collections.sort(numberBox);
+        return numberBox;
     }
 }
