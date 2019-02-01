39c920342895697f05ee4257bb43cac641fb871b
Peng <1136313078@qq.com>
2019-02-01 10:43:42 +0800

    完成关于页面。

--- app/src/main/java/com/peng/lottery/mvp/ui/activity/AboutActivity.java
+++ app/src/main/java/com/peng/lottery/mvp/ui/activity/AboutActivity.java
@@ -1,9 +1,20 @@
 package com.peng.lottery.mvp.ui.activity;
 
+import android.widget.TextView;
+
 import com.peng.lottery.R;
 import com.peng.lottery.base.SimpleBaseActivity;
 
+import butterknife.BindView;
+
 public class AboutActivity extends SimpleBaseActivity {
+
+    @BindView(R.id.tv_about_project_url)
+    TextView tvAboutProjectUrl;
+    @BindView(R.id.tv_about_author_url)
+    TextView tvAboutAuthorUrl;
+    @BindView(R.id.tv_about_version)
+    TextView tvAboutVersion;
 
     @Override
     protected int setLayoutResID() {
@@ -18,7 +29,14 @@ public class AboutActivity extends SimpleBaseActivity {
     }
 
     @Override
+    protected void initListener() {
+        tvAboutProjectUrl.setOnClickListener(v -> WebActivity.start(mActivity, "https://github.com/AndroidPengPeng/Lottery"));
+        tvAboutAuthorUrl.setOnClickListener(v -> WebActivity.start(mActivity, "http://pengblog.club/"));
+    }
+
+    @Override
     protected boolean enableSlidingFinish() {
         return true;
     }
+
 }
